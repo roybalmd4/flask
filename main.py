064b0bd3b3c5837a6b266e1c50d6537eee9764f7
Mike Roybal <mike.roybal@usda.gov>
2020-04-01 14:08:05 -0700

    Created main.py and updated .gitignore to exclude Python venv

---
+++ main.py
@@ -0,0 +1,15 @@
+'''
+    Pull data from AZMET each time the webpage is accessed.  The largest amount of rows that
+    would be imported would be 24x365 (8760)
+'''
+
+import sqlalchemy
+import psycopg2
+import pandas as pd 
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
